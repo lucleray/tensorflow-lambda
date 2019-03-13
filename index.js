995049d698617ae0e778e8d10b309a825f1cf8fd
luc <luc.leray@gmail.com>
2019-03-13 10:02:52 +0100

    export createTfLoader

--- index.js
+++ index.js
@@ -6,51 +6,49 @@ const { resolve } = require('path')
 const requireFunc =
   typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require
 
-let tf
-let tfPromise
-
-async function loadTf(path = '/tmp') {
-  if (tf) {
-    return tf
-  }
-
-  try {
-    const tfjsPath = resolve(path, 'tfjs-node')
-    const tarPath = resolve(__dirname, 'tfjs-node.br')
-
-    if (fs.existsSync(tfjsPath)) {
-      tf = requireFunc(tfjsPath)
-      tf.disableDeprecationWarnings()
-      return tf
-    }
-
-    fs.mkdirSync(tfjsPath)
-
-    // unzip tfjs-node
-    await new Promise((resolve, reject) => {
-      const x = tar.x({ cwd: tfjsPath })
-
-      x.on('finish', resolve)
-      x.on('error', reject)
-
-      fs.createReadStream(tarPath)
-        .pipe(iltorb.decompressStream())
-        .pipe(x)
-    })
-
-    tf = requireFunc(tfjsPath)
-    tf.disableDeprecationWarnings()
-
-    return tf
-  } catch (err) {
-    console.log(err)
-  }
+function isLambda() {
+  return Boolean(process.env['AWS_LAMBDA_FUNCTION_NAME'])
 }
 
-module.exports = path => {
-  if (!tfPromise) {
-    tfPromise = loadTf(path)
+async function createTfPromise(path) {
+  const tfjsPath = resolve(__dirname, path, 'tfjs-node')
+  const tarPath = resolve(__dirname, 'tfjs-node.br')
+
+  if (fs.existsSync(tfjsPath)) {
+    const tf = requireFunc(tfjsPath)
+    tf.disableDeprecationWarnings()
+    return tf
   }
 
-  return tfPromise
+  fs.mkdirSync(tfjsPath)
+
+  // unzip tfjs-node
+  await new Promise((resolve, reject) => {
+    const x = tar.x({ cwd: tfjsPath })
+
+    x.on('finish', resolve)
+    x.on('error', reject)
+
+    fs.createReadStream(tarPath)
+      .pipe(iltorb.decompressStream())
+      .pipe(x)
+  })
+
+  const tf = requireFunc(tfjsPath)
+  tf.disableDeprecationWarnings()
+  return tf
+}
+
+module.exports = async function createTfLoader({
+  path = isLambda() ? '/tmp' : './'
+}) {
+  let tfPromise
+
+  return async function loadTf() {
+    if (!tfPromise) {
+      tfPromise = createTfPromise(path)
+    }
+
+    return tfPromise
+  }
 }
